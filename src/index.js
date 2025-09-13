a2122eb9101841ca18e023d919b5d577f03c6fae
meragittuhub <2024eb02191@online.bits-pilani.ac.in>
2025-09-14 02:46:08 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,81 @@
+// require(' dotenv').config({path: './.env'})
+
+
+import dotenv from "dotenv";  
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: './.env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PROFESSIONAL APPROACH !!!!!
+
+/*
+import express from "express";
+const app = express();
+
+( async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Name}`);
+    
+    app.on("error", (error) => {
+      console.log("Errr:", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    })
+
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error
+  }
+})();
+*/
